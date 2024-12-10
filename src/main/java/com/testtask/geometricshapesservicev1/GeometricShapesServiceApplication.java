41bbfa4dfedcafc43602e99e2381148659adef5f
Vladyslav Kondratenko <1999vlad1999kondratenko@gmail.com>
2024-12-10 18:45:07 +0200

    Init commit

---
+++ src/main/java/com/testtask/geometricshapesservicev1/GeometricShapesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.testtask.geometricshapesservicev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeometricShapesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeometricShapesServiceApplication.class, args);
+    }
+
+}
